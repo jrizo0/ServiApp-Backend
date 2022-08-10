010b27be16ebfc75b76f3f546c9720ef56340fe1
jrizo0 <jr.rizo.o.jr@gmail.com>
2022-08-10 18:23:05 -0500

    ignored cache files

--- ServiApp/usuarios/admin.py
+++ ServiApp/usuarios/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Usuario, Domiciliario, Cliente
+# from .models import Usuario, Domiciliario, Cliente
+from .models import Cliente
 
-admin.site.register(Usuario)
-admin.site.register(Domiciliario)
+# admin.site.register(Usuario)
+# admin.site.register(Domiciliario)
 admin.site.register(Cliente)
