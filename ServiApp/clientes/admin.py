38f2b6af0c15e1e81c5d7a7a662f59a8172c5873
jrizo0 <jr.rizo.o.jr@gmail.com>
2022-08-12 11:59:34 -0500

    cliente app - urls serializers

--- ServiApp/clientes/admin.py
+++ ServiApp/clientes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cliente
+
+# admin.site.register(Cliente)
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('codcliente', 'nombrecliente', 'e_mail')
+    # list_filter = ['nombrecliente']
+    search_fields = ['nombrecliente']
+
+admin.site.register(Cliente, ClienteAdmin)
