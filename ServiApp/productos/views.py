3ea1c30a2a782c73804f4bfa51a0352a610a479b
jrizo0 <jr.rizo.o.jr@gmail.com>
2022-09-26 18:45:48 -0500

    update views

--- ServiApp/productos/views.py
+++ ServiApp/productos/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 import requests
 
 from rest_framework.response import Response
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
@@ -15,13 +16,37 @@ from django.core.exceptions import PermissionDenied
 
 
 API_Productos = settings.SA_API_URL + "/productos/"
+API_Tarifas = settings.SA_API_URL + "/tarifas/"
 
-# NOTE: No se puede @api_view por cache solo se puede con clases).
+class FBAuthenticated(BasePermission):
+    def __init__(self):
+        self.enable_auth = False
+
+    def has_permission(self, request, view):
+        return not self.enable_auth or fb_valid_req_token(request)
+
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
+
 class ProductosAPIView(
     viewsets.GenericViewSet,
 ):
-    def __init__(self):
-        self.enable_auth = False
+    permission_classes = [ReadOnly|FBAuthenticated]
+
+    def aux_fill_missing_fields(self, prods, fs_query_cats):
+        for i in range(len(prods)):
+            if prods[i]["Imagen"] != "" and prods[i]["Descripcion"] != "":
+                continue
+            for doc_cat in fs_query_cats:
+                if doc_cat.id != prods[i]["Categoria"]:
+                    continue
+                if prods[i]["Imagen"] == "":
+                    prods[i] = prods[i] | {"Imagen": doc_cat.to_dict()["Imagen"]}
+                if prods[i]["Descripcion"] == "":
+                    prods[i] = prods[i] | {"Descripcion": doc_cat.to_dict()["Descripcion"]}
+        return prods
 
     # NOTE: Queryset el nombre de ServiciosAlimentacionApi.
     # def get_queryset(self):
@@ -39,8 +64,10 @@ class ProductosAPIView(
 
     # NOTE: Queryset guardando el nombre en fb.
     def get_queryset(self):
-        fs_query = db.collection("Producto").get()
-        prods = [{"id": doc.id} | doc.to_dict() for doc in fs_query]
+        fs_query_prods = db.collection("Producto").get()
+        fs_query_cats = db.collection("CategoriaProducto").get()
+        prods = [{"id": doc.id} | doc.to_dict() for doc in fs_query_prods]
+        prods = self.aux_fill_missing_fields(prods, fs_query_cats)
         return prods
 
     # @method_decorator(vary_on_cookie)
@@ -51,37 +78,34 @@ class ProductosAPIView(
     # @method_decorator(vary_on_headers("Authorization"))
     # @method_decorator(vary_on_cookie)
     # @method_decorator(cache_page(60 * 1))
+    def list_rest(self, request, id_rest):
+        if "20-" in id_rest: id_rest = "20"
+        tarifas_api = requests.get(API_Tarifas + "tarifav/" + id_rest + "/").json()
+        fs_query_prods = db.collection("Producto").get()
+        rests = []
+        for prod in fs_query_prods:
+            if not prod.id in tarifas_api:
+                continue
+            rests.append({"id": prod.id} | prod.to_dict() | {"Precio": tarifas_api[prod.id]["precio"]})
+        fs_query_cats = db.collection("CategoriaProducto").get()
+        rests = self.aux_fill_missing_fields(rests, fs_query_cats)
+        return Response(rests)
+
+    # TODO : Eliminar vista?
+    # @method_decorator(vary_on_headers("Authorization"))
+    # @method_decorator(vary_on_cookie)
+    # @method_decorator(cache_page(60 * 1))
     def list_category(self, request, id_category):
-        if self.enable_auth and not fb_valid_req_token(request):
-            raise PermissionDenied()
         fs_query = db.collection("Producto").where("Categoria", "==", id_category).get()
         rests_in_category = [{"id": doc.id} | doc.to_dict() for doc in fs_query]
         return Response(rests_in_category)
 
-    # @method_decorator(vary_on_headers("Authorization"))
-    # @method_decorator(vary_on_cookie)
-    # @method_decorator(cache_page(60 * 1))
-    def list_rest(self, request, id_rest):
-        if self.enable_auth and not fb_valid_req_token(request):
-            raise PermissionDenied()
-        prods_in_rest = []
-        fs_query = db.collection("Tarifa").where("Restaurante", "==", id_rest).get()
-        for prod in fs_query:
-            prod_info = (
-                db.collection("Producto").document(prod.to_dict()["Producto"]).get()
-            )
-            prod = {"id": prod_info.id} | prod.to_dict() | prod_info.to_dict()
-            del prod["Producto"], prod["Restaurante"]
-            prods_in_rest.append(prod)
-        return Response(prods_in_rest)
-
+    # TODO : Eliminar vista?
     # @method_decorator(vary_on_cookie)
     # @method_decorator(cache_page(60 * 1))
     def list_rest_by_category(self, request, id_rest):
-        if self.enable_auth and not fb_valid_req_token(request):
-            raise PermissionDenied()
         prods_in_rest = []
-        fs_query = db.collection("Tarifa").where("Restaurante", "==", id_rest).get()
+        fs_query = db.collection("Producto").where("Restaurante", "==", id_rest).get()
         for prod in fs_query:
             prod_info = (
                 db.collection("Producto").document(prod.to_dict()["Producto"]).get()
@@ -89,11 +113,9 @@ class ProductosAPIView(
             prod = {"id": prod_info.id} | prod.to_dict() | prod_info.to_dict()
             del prod["Producto"], prod["Restaurante"]
             prods_in_rest.append(prod)
-
         categories = set(prod["Categoria"] for prod in prods_in_rest)
         prods_by_cat = {
             cat: [prod for prod in prods_in_rest if prod["Categoria"] == cat]
             for cat in categories
         }
-
         return Response(prods_by_cat)
