cfbca3b9497db85e1edd5d1737fe770a5ee37aa7
jrizo0 <jr.rizo.o.jr@gmail.com>
2022-10-05 15:21:36 -0500

    disable scheduler

--- ServiApp/recomendaciones/updater.py
+++ ServiApp/recomendaciones/updater.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from .jobs import schedule_api
 
-def start():
-	scheduler = BackgroundScheduler()
-	scheduler.add_job(schedule_api, 'cron', hour=13, minute=48, timezone='America/Bogota')
-	scheduler.start()
+# def start():
+	# scheduler = BackgroundScheduler()
+	# scheduler.add_job(schedule_api, 'cron', hour=13, minute=48, timezone='America/Bogota')
+	# scheduler.start()
