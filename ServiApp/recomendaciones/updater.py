1c06ef59fd24596227e293df8dc7dea4ea563256
jrizo0 <jr.rizo.o.jr@gmail.com>
2022-11-02 01:16:38 -0500

    update

--- ServiApp/recomendaciones/updater.py
+++ ServiApp/recomendaciones/updater.py
@@ -5,4 +5,5 @@ from .jobs import schedule_api
 # def start():
 	# scheduler = BackgroundScheduler()
 	# scheduler.add_job(schedule_api, 'cron', hour=13, minute=48, timezone='America/Bogota')
+	# scheduler.add_job(schedule_api, 'interval', seconds=5)
 	# scheduler.start()
